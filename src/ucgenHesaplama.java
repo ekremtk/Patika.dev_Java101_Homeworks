0d0c7aba0823d49684bbc389d7fd6c6c42bb8260
etk <ekremkaran@marun.edu.tr>
2022-07-07 09:21:25 +0300

    Hipotenüs / Çevre / Alan Ödevi

---
+++ src/ucgenHesaplama.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ucgenHesaplama {
+    public static void main(String[] args) {
+        // Üç kenar uzunluğunu kullanıcıdan aldığınız
+        // üçgenin alanını hesaplayan programı yazınız.
+
+        double kenar1, kenar2, hipotenus, alan, cevre;
+
+        // Scanner Sınıfını tamamladık
+        Scanner girdi = new Scanner(System.in);
+
+        // Kullanıcadan  Kenarların Değerlerini Al
+        System.out.print("1. Kenar Uzunluğunu Giriniz: ");
+        kenar1 = girdi.nextInt();
+
+        System.out.print("2. Kenar Uzunluğunu Giriniz: ");
+        kenar2 = girdi.nextInt();
+
+        // Hipotenus
+        hipotenus = Math.sqrt((Math.pow(kenar1, 2)) + (Math.pow(kenar2, 2)));
+
+        // Çevre ve Alan Hesaplamaları
+        cevre = kenar1 + kenar2 + hipotenus;
+
+        alan = (kenar1 * kenar2) / 2;
+
+        System.out.println("Girilen Değerlere Göre Dik Üçgenin Değerleri");
+        System.out.println("Hipotenüs Uzunluğu: " + hipotenus);
+        System.out.println("Çevresi: " + cevre);
+        System.out.println("Alanı: " + alan);
+    }
+}
