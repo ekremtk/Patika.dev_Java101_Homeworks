ab1d17ce90b1f2a2a554e056fc56e6be533afab6
etk <ekremkaran@marun.edu.tr>
2022-07-07 10:23:59 +0300

    VKI Ödevi

---
+++ src/vucutKitleIndeks.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class vucutKitleIndeks {
+    public static void main(String[] args) {
+        // Vücut Kitle İndeksi Hesaplama
+        // Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın.
+        // Aşağıdaki formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
+
+        double kilo, boy, vki;
+
+        // Scanner Sınıfını tamamladık
+        Scanner girdi = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz: ");
+        boy = girdi.nextDouble();
+
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        kilo = girdi.nextDouble();
+
+        vki = kilo / (boy * boy);
+
+        System.out.println("Vücut Kitle İndeksiniz: " + vki);
+    }
+}
